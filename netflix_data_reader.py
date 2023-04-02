07c989742904e8a4473c1cae2e96c2b2aea36039
luisgutzeit <luis@s7d.io>
2023-04-02 21:38:19 +0200

    added ability to split test data

--- netflix_data_reader.py
+++ netflix_data_reader.py
@@ -19,11 +19,11 @@ class NetflixReader:
                            "imdb_score": "float",
                            "imdb_votes": "int"
                            }
-        self._data_split_ratios = {"train": 0.8, "val": 0.2, "test": 0}
+        self._data_split_ratios = {"train": 0.4, "val": 0.3, "test": 0.3}
         self.data_leakage_warning = False
 
     def set_data_split_ratio(self, new_split_ratios: dict):
-        pass
+        self._data_split_ratios = new_split_ratios
 
     def read_netflix_data(self, file_path: str):
         self.netflix_data_raw = pd.read_csv(file_path, sep=">")
@@ -56,7 +56,18 @@ class NetflixReader:
         pass
 
     def _split_data(self):
-        pass
+        #shuffle the rows so that the data is not ordered by any column
+        data_shuffled = self.netflix_data_raw.sample(frac=1)
+
+        #calculate the number of rows for each split based on the split ratios
+        train_rows = int(data_shuffled.shape[0] * self._data_split_ratios["train"])
+        val_rows = int(data_shuffled.shape[0] * self._data_split_ratios["val"])
+        test_rows = int(data_shuffled.shape[0] * self._data_split_ratios["test"])
+
+        #take the rows acording to the split ratios and fill the dataframes
+        self.train_data = data_shuffled.iloc[:train_rows]   
+        self.val_data = data_shuffled.iloc[train_rows:train_rows + val_rows]
+        self.test_data = data_shuffled.iloc[train_rows + val_rows:]
 
     def _is_data_leakage(self):
         pass

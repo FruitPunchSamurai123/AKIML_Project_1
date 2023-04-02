29c29a3ed66744c05253ccdd8676fc13982a7202
nbinder <nbinder@hs-heilbronn.de>
2023-04-02 14:04:11 +0200

    delete unnecessary blank line in main.py

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ import netflix_data_reader as reader
 netflix_reader = reader.NetflixReader()
 netflix_reader.read_netflix_data(file_path="./files/netflix_data.csv")
 print(netflix_reader.netflix_data_raw)
-
